436cb82e72250b141ac8f61a46ace96ede2df3a6
Andrew W. Donoho <awd@ddg.com>
2012-10-20 17:09:17 -0500

    Add methods similar to those in the NSValue UIKit additions to NSData.

--- DDGPreferencesApp/DDGPreferences/Preferences/NSData+DDGValue.h
+++ DDGPreferencesApp/DDGPreferences/Preferences/NSData+DDGValue.h
@@ -8,11 +8,16 @@
 
 /*
  
- Based upon ideas and code described by Steffen Itterheim, <http://stackoverflow.com/users/201863/learncocos2d>,
- on Stack Overflow at: <http://stackoverflow.com/questions/8447380/how-to-convert-nsvalue-to-nsdata-and-back>.
+ As NSUbiquitousKeyValueStore does not store NSValues, I need NSData items for common
+ structures. Hence, I extended NSData to cover all of the UIKit additions to NSValue.
  
- As with all Stack Overflow contributions, Steffen's code is used under terms specified by the Creative Commons
- Attribution-ShareAlike 3.0 Unported (CC BY-SA 3.0) license: <http://creativecommons.org/licenses/by-sa/3.0/>.
+ Based upon ideas and code described by Steffen Itterheim,
+ <http://stackoverflow.com/users/201863/learncocos2d>, on Stack Overflow at:
+ <http://stackoverflow.com/questions/8447380/how-to-convert-nsvalue-to-nsdata-and-back>.
+ 
+ As with all Stack Overflow contributions, <http://stackoverflow.com/faq#editing>, Steffen's
+ code is used under terms specified by the Creative Commons Attribution-ShareAlike 3.0
+ Unported (CC BY-SA 3.0) license: <http://creativecommons.org/licenses/by-sa/3.0/>.
  
  */
 
@@ -22,5 +27,29 @@
 
 + (NSData *) dataWithValue: (NSValue *) value;
 - (NSData *) initWithValue: (NSValue *) value;
+
++ (NSData *) dataWithCGAffineTransform: (CGAffineTransform) transform;
+- (NSData *) initWithCGAffineTransform: (CGAffineTransform) transform;
+- (CGAffineTransform) CGAffineTransformValue;
+
++ (NSData *) dataWithCGPoint: (CGPoint) point;
+- (NSData *) initWithCGPoint: (CGPoint) point;
+- (CGPoint)  CGPointValue;
+
++ (NSData *) dataWithCGRect: (CGRect) rect;
+- (NSData *) initWithCGRect: (CGRect) rect;
+- (CGRect)   CGRectValue;
+
++ (NSData *) dataWithCGSize: (CGSize) size;
+- (NSData *) initWithCGSize: (CGSize) size;
+- (CGSize)   CGSizeValue;
+
++ (NSData *) dataWithUIEdgeInsets: (UIEdgeInsets) insets;
+- (NSData *) initWithUIEdgeInsets: (UIEdgeInsets) insets;
+- (UIEdgeInsets) UIEdgeInsetsValue;
+
++ (NSData *) dataWithUIOffset: (UIOffset) offset;
+- (NSData *) initWithUIOffset: (UIOffset) offset;
+- (UIOffset) UIOffsetValue;
 
 @end
--- DDGPreferencesApp/DDGPreferences/Preferences/NSData+DDGValue.m
+++ DDGPreferencesApp/DDGPreferences/Preferences/NSData+DDGValue.m
@@ -8,11 +8,16 @@
 
 /*
  
- Based upon ideas and code described by Steffen Itterheim, <http://stackoverflow.com/users/201863/learncocos2d>,
- on Stack Overflow at: <http://stackoverflow.com/questions/8447380/how-to-convert-nsvalue-to-nsdata-and-back>.
+ As NSUbiquitousKeyValueStore does not store NSValues, I need NSData items for common
+ structures. Hence, I extended NSData to cover all of the UIKit additions to NSValue.
  
- As with all Stack Overflow contributions, Steffen's code is used under terms specified by the Creative Commons 
- Attribution-ShareAlike 3.0 Unported (CC BY-SA 3.0) license: <http://creativecommons.org/licenses/by-sa/3.0/>.
+ Based upon ideas and code described by Steffen Itterheim,
+ <http://stackoverflow.com/users/201863/learncocos2d>, on Stack Overflow at: 
+ <http://stackoverflow.com/questions/8447380/how-to-convert-nsvalue-to-nsdata-and-back>.
+ 
+ As with all Stack Overflow contributions, <http://stackoverflow.com/faq#editing>, Steffen's 
+ code is used under terms specified by the Creative Commons Attribution-ShareAlike 3.0 
+ Unported (CC BY-SA 3.0) license: <http://creativecommons.org/licenses/by-sa/3.0/>.
  
  */
 
@@ -28,7 +33,7 @@
     
     return [self.alloc initWithValue: value];
 
-} // -dataWithValue:
+} // +dataWithValue:
 
 
 - (NSData *) initWithValue: (NSValue *) value {
@@ -44,5 +49,131 @@
     return (self = [self initWithBytesNoCopy: valueData length: size freeWhenDone: YES]);
     
 } // -initWithValue:
+
+
++ (NSData *) dataWithCGAffineTransform: (CGAffineTransform) transform {
+    
+    return [self.alloc initWithCGAffineTransform: transform];
+    
+} // -dataWithCGAffineTransform:
+
+
+- (NSData *) initWithCGAffineTransform: (CGAffineTransform) transform {
+    
+    return (self = [self initWithBytes: &transform length: sizeof(CGAffineTransform)]);
+    
+} // -initWithCGAffineTransform:
+
+
+- (CGAffineTransform) CGAffineTransformValue {
+    
+    return *(CGAffineTransform *)(self.bytes);
+    
+} // -CGAffineTransformValue
+
+
++ (NSData *) dataWithCGPoint: (CGPoint) point {
+    
+    return [self.alloc initWithCGPoint: point];
+    
+} // -dataWithCGPoint:
+
+
+- (NSData *) initWithCGPoint: (CGPoint) point {
+    
+    return (self = [self initWithBytes: &point length: sizeof(CGPoint)]);
+    
+} // -initWithCGPoint:
+
+
+- (CGPoint) CGPointValue {
+    
+    return *(CGPoint *)(self.bytes);
+    
+} // -CGPointValue
+
+
++ (NSData *) dataWithCGRect: (CGRect) rect {
+    
+    return [self.alloc initWithCGRect: rect];
+    
+} // -dataWithCGRect:
+
+
+- (NSData *) initWithCGRect: (CGRect) rect {
+    
+    return (self = [self initWithBytes: &rect length: sizeof(CGRect)]);
+    
+} // -initWithCGRect:
+
+
+- (CGRect) CGRectValue {
+    
+    return *(CGRect *)(self.bytes);
+    
+} // -CGRectValue
+
+
++ (NSData *) dataWithCGSize: (CGSize) size {
+    
+    return [self.alloc initWithCGSize: size];
+    
+} // -dataWithCGSize:
+
+
+- (NSData *) initWithCGSize: (CGSize) size {
+    
+    return (self = [self initWithBytes: &size length: sizeof(CGSize)]);
+    
+} // -initWithCGSize:
+
+
+- (CGSize) CGSizeValue {
+    
+    return *(CGSize *)(self.bytes);
+    
+} // -CGSizeValue
+
+
++ (NSData *) dataWithUIEdgeInsets: (UIEdgeInsets) insets {
+    
+    return [self.alloc initWithUIEdgeInsets: insets];
+    
+} // -dataWithUIEdgeInsets:
+
+
+- (NSData *) initWithUIEdgeInsets: (UIEdgeInsets) insets {
+    
+    return (self = [self initWithBytes: &insets length: sizeof(UIEdgeInsets)]);
+    
+} // -initWithUIEdgeInsets:
+
+
+- (UIEdgeInsets) UIEdgeInsetsValue {
+    
+    return *(UIEdgeInsets *)(self.bytes);
+    
+} // -UIEdgeInsetsValue
+
+
++ (NSData *) dataWithUIOffset: (UIOffset) offset {
+    
+    return [self.alloc initWithUIOffset: offset];
+    
+} // -dataWithUIOffset:
+
+
+- (NSData *) initWithUIOffset: (UIOffset) offset {
+    
+    return (self = [self initWithBytes: &offset length: sizeof(UIOffset)]);
+    
+} // -initWithUIOffset:
+
+
+- (UIOffset) UIOffsetValue {
+    
+    return *(UIOffset *)(self.bytes);
+    
+} // -UIOffsetValue
 
 @end
