b869b3fe1e6d4ce97118e8092fa95a89e1c3caaf
Andrew W. Donoho <awd@ddg.com>
2012-10-22 10:57:40 -0500

    Improve the justification comments.

--- DDGPreferencesApp/DDGPreferences/Preferences/NSData+DDGValue.m
+++ DDGPreferencesApp/DDGPreferences/Preferences/NSData+DDGValue.m
@@ -8,16 +8,55 @@
 
 /*
  
- As NSUbiquitousKeyValueStore does not store NSValues, I need NSData items for common
- structures. Hence, I extended NSData to cover all of the UIKit additions to NSValue.
- 
- Based upon ideas and code described by Steffen Itterheim,
+ As NSUbiquitousKeyValueStore does not store NSValues, it only supports .plist 
+ compatible data types. Hence, I extended NSData to cover all of the UIKit 
+ additions to NSValue. I also incorporated a general mechanism to convert an 
+ arbitrary NSValue into an NSData. Those methods, +dataWithValue:/-initWithValue:, 
+ are based upon ideas described by Steffen Itterheim,
  <http://stackoverflow.com/users/201863/learncocos2d>, on Stack Overflow at: 
  <http://stackoverflow.com/questions/8447380/how-to-convert-nsvalue-to-nsdata-and-back>.
  
- As with all Stack Overflow contributions, <http://stackoverflow.com/faq#editing>, Steffen's 
- code is used under terms specified by the Creative Commons Attribution-ShareAlike 3.0 
- Unported (CC BY-SA 3.0) license: <http://creativecommons.org/licenses/by-sa/3.0/>.
+ As with all Stack Overflow contributions, <http://stackoverflow.com/faq#editing>, 
+ Steffen's code is used under terms specified by the Creative Commons 
+ Attribution-ShareAlike 3.0 Unported (CC BY-SA 3.0) license: 
+ <http://creativecommons.org/licenses/by-sa/3.0/>.
+ 
+ */
+
+/*
+ 
+ The below license is the new BSD license with the OSI recommended
+ personalizations.
+ <http://www.opensource.org/licenses/bsd-license.php>
+ 
+ Copyright (C) 2010-2012 Donoho Design Group, LLC. All Rights Reserved.
+ 
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are
+ met:
+ 
+ * Redistributions of source code must retain the above copyright notice,
+ this list of conditions and the following disclaimer.
+ 
+ * Redistributions in binary form must reproduce the above copyright
+ notice, this list of conditions and the following disclaimer in the
+ documentation and/or other materials provided with the distribution.
+ 
+ * Neither the name of Andrew W. Donoho nor Donoho Design Group, L.L.C.
+ may be used to endorse or promote products derived from this software
+ without specific prior written permission.
+ 
+ THIS SOFTWARE IS PROVIDED BY DONOHO DESIGN GROUP, L.L.C. "AS IS" AND ANY
+ EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  
  */
 
