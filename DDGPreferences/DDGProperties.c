cce49bda3f3403f09f003f323dccc0bc35e241a6
Andrew W. Donoho <github@store.ddg.com>
2014-08-31 09:31:00 -0500

    Add the supported property name checking code and remove the now unused value wrapping code.

---
+++ DDGPreferences/DDGProperties.c
@@ -0,0 +1,9 @@
+//
+//  DDGProperties.c
+//  DDGPreferences
+//
+//  Created by Andrew Donoho on 2014/08/30.
+//  Copyright (c) 2014 Donoho Design Group, L.L.C. All rights reserved.
+//
+
+#include <stdio.h>
---
+++ DDGPreferences/DDGProperties.h
@@ -0,0 +1,14 @@
+//
+//  DDGProperties.h
+//  DDGPreferences
+//
+//  Created by Andrew Donoho on 2014/08/30.
+//  Copyright (c) 2014 Donoho Design Group, L.L.C. All rights reserved.
+//
+
+#ifndef DDGPreferences_DDGProperties_h
+#define DDGPreferences_DDGProperties_h
+
+
+
+#endif
